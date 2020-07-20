551ada6a233fa1c1dcd01ec66517dba47b04d5bb
pretendent <dovydaspacesa@gmail.com>
2020-07-20 17:17:15 +0300

    added files, fa icons

--- src/public/index.php
+++ src/public/index.php
@@ -1,18 +1,19 @@
-<?php require "../back/create.php" ?>
-<?php require "../back/delete.php" ?>
+<?php require "../includes/create.php" ?>
+<?php require "../includes/delete.php" ?>
 <?php
 $path = "./";
 $files = scandir($path);
-echo ('<table class="table"><th>Type</th><th>Name</th><th>Actions</th>');
+echo ('<table class="table">
+<th>File</th>
+ <th>Actions</th>');
 foreach ($files as $file){
         if($file != '.' && $file != '..') {
             echo ('<tr>');
             if(is_dir($path . $file)){
-                echo "<td>Directory</td>";
+                echo ('<td>'. '<i class="fa fa-folder"></i>' . "<a href='$file'> $file" . '</a></td>');
             }else{
-                echo "<td>File</td>";
+                echo ('<td>' . '<i class="fa fa-file-code-o"></i>'. $file . '</td>');
             }
-            echo ( '<td>' . "<a href='$file'> $file" . '</a></td>');
             if(is_dir($path . $file)){
                 echo '<td> </td>';
             }else{
@@ -22,9 +23,6 @@ foreach ($files as $file){
                         <form id="actions" action="\" method="POST">
                         <input type="hidden" name="download" value="">
                         <input type="submit" value="Download"></form>
-                        <form id="actions" action="\" method="POST">
-                        <input type="hidden" name="upload" value="">
-                        <input type="submit" value="Upload">
                         </form></td>';
             }
         }
@@ -38,7 +36,7 @@ foreach ($files as $file){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    <link rel="stylesheet" href="style.css">
 
     <title>Document</title>
 </head>
@@ -46,11 +44,12 @@ foreach ($files as $file){
     <form class="create" action="index.php" method="GET">
         <input class="docname" placeholder="Name of new directory" type="text" id="create" name="create">
         <button class="push" type="submit">Create Directory</button>
-        <button class="push"><a href="index.php"></a>HOME</button>
+<!--        <button class="push"><a href="index.php"></a>HOME</button>-->
     <form class="create" action="index.php" method="GET">
         <input class="docname" placeholder="Name of new file" type="text" name="createfile">
         <button class="push" type="submit">Create File</button>
     </form>
+
 </form>
 </body>
 </html>
